d26db7243b0d1d695d60e38c7758c3d5eb80336e
Dominic Stait <dominic.stait@genomicsengland.co.uk>
2025-09-25 11:39:08 +0100

    updated lualine settings

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -5,10 +5,18 @@ return {
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
 		options = {
-			icons_enabled = false,
+			icons_enabled = true,
 			theme = 'neon',
 			component_separators = '|',
 			section_separators = '',
+		},
+		sections = {
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_c = { { 'filename', path = 1, } },
+			lualine_x = { 'encoding', 'fileformat', 'filetype' },
+			lualine_y = { 'progress' },
+			lualine_z = { 'location' }
 		},
 	},
 }
