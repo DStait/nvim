9fcb8c1facc74015db6201c0f85e0a266a6bb36c
Dominic Stait <dominic.stait@genomicsengland.co.uk>
2025-09-25 11:37:26 +0100

    misspelling remaps for q

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -3,3 +3,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 
 vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
