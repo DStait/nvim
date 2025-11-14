df0a2c519bb50847cc08d1e3f9a47f5a6528ece1
Dominic Stait <dominic.stait@genomicsengland.co.uk>
2025-11-14 15:42:09 +0000

    write without formatting

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -6,9 +6,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-vim.api.nvim_create_user_command('WQ', 'wq', {})
-vim.api.nvim_create_user_command('Wq', 'wq', {})
-vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('W', 'noa w', {})
 vim.api.nvim_create_user_command('Qa', 'qa', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 
