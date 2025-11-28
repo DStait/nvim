c4d0f400d632c15e2767fd4e7dd1830163424b91
Dominic Stait <dstait@mailbox.org>
2025-11-28 13:47:36 +0000

    gitsigns actions removed

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -9,68 +9,6 @@ return {
 			delete = { text = '_' },
 			topdelete = { text = '‾' },
 			changedelete = { text = '~' },
-		},
-		on_attach = function(bufnr)
-			local gs = package.loaded.gitsigns
-
-			local function map(mode, l, r, opts)
-				opts = opts or {}
-				opts.buffer = bufnr
-				vim.keymap.set(mode, l, r, opts)
-			end
-
-			-- Navigation
-			map({ 'n', 'v' }, ']c', function()
-				if vim.wo.diff then
-					return ']c'
-				end
-				vim.schedule(function()
-					gs.next_hunk()
-				end)
-				return '<Ignore>'
-			end, { expr = true, desc = 'Jump to next hunk' })
-
-			map({ 'n', 'v' }, '[c', function()
-				if vim.wo.diff then
-					return '[c'
-				end
-				vim.schedule(function()
-					gs.prev_hunk()
-				end)
-				return '<Ignore>'
-			end, { expr = true, desc = 'Jump to previous hunk' })
-
-			-- Actions
-			-- visual mode
-			map('v', '<leader>hs', function()
-				gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-			end, { desc = 'stage git hunk' })
-			map('v', '<leader>hr', function()
-				gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-			end, { desc = 'reset git hunk' })
-			--
-			-- normal mode
-			map('n', '<leader>ghs', gs.stage_hunk, { desc = 'stage hunk' })
-			map('n', '<leader>ghr', gs.reset_hunk, { desc = 'reset hunk' })
-			map('n', '<leader>ghS', gs.stage_buffer, { desc = 'Stage buffer' })
-			map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-			map('n', '<leader>ghR', gs.reset_buffer, { desc = 'Reset buffer' })
-			map('n', '<leader>ghp', gs.preview_hunk, { desc = 'preview git hunk' })
-			map('n', '<leader>ghb', function()
-				gs.blame_line { full = false }
-			end, { desc = 'blame line' })
-			map('n', '<leader>ghd', gs.diffthis, { desc = 'diff against index' })
-			map('n', '<leader>ghB', gs.blame, { desc = '[B]lame in diff' })
-			map('n', '<leader>ghD', function()
-				gs.diffthis '~'
-			end, { desc = 'diff against last commit' })
-
-			-- Toggles
-			map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-			map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-			-- Text object
-			map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-		end,
+		}
 	},
 }
